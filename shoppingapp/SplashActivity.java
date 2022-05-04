26b7f06d913733c4a1c06ffc4582b036dfc81ad1
Mahesh_Kanigiri <79745244+mahesh091@users.noreply.github.com>
2022-05-04 15:08:41 +0530

    layout (.xml) and Activity (.java) files

---
+++ shoppingapp/SplashActivity.java
@@ -0,0 +1,41 @@
+package com.example.shoppingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity
+{
+    Animation topA,bottomA;
+    ImageView img;
+    TextView s_title,title_txt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run()
+            {
+                Intent intent =new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        },4500);
+
+
+    }
+}
